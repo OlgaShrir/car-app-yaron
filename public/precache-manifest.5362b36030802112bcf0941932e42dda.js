5273fbeb7bc9930846e2309d90d1a31a2dbf6a06
Olga <olga.shrir@gmail.com>
2019-03-18 10:36:45 +0200

    first

---
+++ public/precache-manifest.5362b36030802112bcf0941932e42dda.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "95ad0f877f7f535013d9",
+    "url": "/css/app.ec1ddd36.css"
+  },
+  {
+    "revision": "95ad0f877f7f535013d9",
+    "url": "/js/app.33dc928e.js"
+  },
+  {
+    "revision": "213191469a58694691a9",
+    "url": "/js/chunk-vendors.fe7bdd87.js"
+  },
+  {
+    "revision": "aed851afb193fc4ee757d349220ced1c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+];
